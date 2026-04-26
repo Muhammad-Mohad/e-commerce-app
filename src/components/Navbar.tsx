0b36e23985d9c4ee56f5ec7d609f72c4933e2f98
rustamali9900 <144158447+rustamali9900@users.noreply.github.com>
2026-04-26 23:35:36 +0500

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,35 @@
 import { Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { useCartStore } from "../store/cartStore";
 
 export default function Navbar() {
+  const items = useCartStore((s: any) => s.items);
+
   return (
     <View style={styles.container}>
       <NavItem icon="home" label="Home" active />
       <NavItem icon="storefront" label="Shop" />
-      <NavItem icon="cart" label="Cart" />
+
+      <View>
+        <NavItem icon="cart" label="Cart" />
+        {items.length > 0 && (
+          <View style={styles.badge}>
+            <Text style={styles.badgeText}>{items.length}</Text>
+          </View>
+        )}
+      </View>
+
       <NavItem icon="person" label="Profile" />
     </View>
   );
 }
 
-function NavItem({ icon, label, active = false }: any) {
+function NavItem({ icon, label, route, active = false }: any) {
+  const router = useRouter();
+
   return (
-    <TouchableOpacity style={styles.item}>
+    <TouchableOpacity style={styles.item} onPress={() => router.push(route)}>
       <Ionicons name={icon} size={22} color={active ? "#fff" : "#888"} />
       <Text style={[styles.label, active && { color: "#fff" }]}>{label}</Text>
     </TouchableOpacity>
@@ -33,12 +48,28 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     borderRadius: 30,
   },
+
   item: {
     alignItems: "center",
   },
+
   label: {
     fontSize: 10,
     color: "#888",
     marginTop: 2,
+  },
+
+  badge: {
+    position: "absolute",
+    top: -5,
+    right: 15,
+    backgroundColor: "red",
+    borderRadius: 10,
+    paddingHorizontal: 6,
+  },
+
+  badgeText: {
+    color: "#fff",
+    fontSize: 10,
   },
 });
