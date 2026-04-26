2737f5f466bdd079d92fd7fcd4ccc88c18b73631
rustamali9900 <144158447+rustamali9900@users.noreply.github.com>
2026-04-26 20:20:34 +0500

    importing font and making auth and main routes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,12 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  const [loaded] = useFonts({
+    Rosemary: require("../assets/fonts/Rosemary-Regular.ttf"),
+  });
+
+  if (!loaded) return null;
+
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
